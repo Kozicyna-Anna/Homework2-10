bf2bbb53e11df97b8ad64356978ebf61f43cd2ec
New <Dar1985@bk.ru>
2024-09-04 23:20:11 +0700

    Проверить ДЗ

--- src/main/java/org/example/InvalidInputException.java
+++ src/main/java/org/example/InvalidInputException.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class InvalidInputException {
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidInputException extends RuntimeException {
+
+public InvalidInputException(String message) {
+    super(message);
+}
+public InvalidInputException(String message, Throwable cause) {
+
+}
+public InvalidInputException(Throwable cause) {
+    super(cause);
+}
+public InvalidInputException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+}
 }
